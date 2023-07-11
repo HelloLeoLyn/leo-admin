ad33bd04b332123bda56ab3983356a661d1c617d
HelloLeoLyn <541124712@qq.com>
2023-07-11 16:37:34 +0800

    修改图片管理功能

--- src/api/leo-photo.js
+++ src/api/leo-photo.js
@@ -7,23 +7,18 @@ export function api_page_image(data) {
     data
   })
 }
-export function api_photo_alibaba_album_add(data) {
-  return request({
-    url: 'http://localhost:8080/photo/alibaba/album/add',
-    method: 'post',
-    data
-  })
-}
-export function api_photo_alibaba_upload(data) {
-  return request({
-    url: service + '/photo/alibaba/uploadOne',
-    method: 'post',
-    data
-  })
-}
+
 export function api_photo_alibaba_album(data) {
   return request({
     url: 'http://localhost:8080/photo/alibaba/album',
+    method: 'post',
+    data
+  })
+}
+
+export function api_photo_alibaba_photo(data) {
+  return request({
+    url: 'http://localhost:8080/photo/alibaba/photo',
     method: 'post',
     data
   })
